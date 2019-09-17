e280460d724ef668e14b79321e6dc7023e2b0267
Mithil.Kumar <mithil.kumar@MithilKumars-MacBook-Pro.local>
2019-09-17 16:09:06 +0530

    [Mithil] Add. Create Booking class and show list of movies.

---
+++ src/main/java/Booking.java
@@ -0,0 +1,13 @@
+import data.Movies;
+
+public class Booking {
+    private final Movies movies;
+
+    public Booking(Movies movies) {
+        this.movies=movies;
+    }
+
+    public void listOfMovies() {
+        movies.setMovies();
+    }
+}
---
+++ src/test/java/BookingTest.java
@@ -0,0 +1,15 @@
+import data.Movies;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class BookingTest {
+    @Test
+    void expectListOfMoviesToReturnWhenBookIsCalledInBooking(){
+        Movies movies=mock(Movies.class);
+        Booking bookTheShow=new Booking(movies);
+        bookTheShow.listOfMovies();
+        verify(movies).setMovies();
+    }
+}
